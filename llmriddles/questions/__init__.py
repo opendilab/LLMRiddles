46ad8888399037a03b9f0646ecdc81343d40a07f
Norman <74552232+TuTuHuss@users.noreply.github.com>
2023-11-07 23:42:53 +0800

    style(hus): add chapter 5 question and polish readme (#3)
    * style(hus):add new chapter and new question
    
    * style(hus):polish readme(add submit custom level)
    
    * style(hus):fix chapter name
    
    ---------
    
    Co-authored-by: Swain <niuyazhe@sensetime.com>

--- llmriddles/questions/__init__.py
+++ llmriddles/questions/__init__.py
@@ -2,8 +2,10 @@ from .executor import QuestionExecutor
 from .level1 import __file__ as _level1_file_
 from .level3 import __file__ as _level3_file_
 from .level4 import __file__ as _level4_file_
+from .level5 import __file__ as _level5_file_
 from .question import Question, register_question, list_ordered_questions
 
 _ = _level1_file_
 _ = _level3_file_
 _ = _level4_file_
+_ = _level5_file_
