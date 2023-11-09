719bd697be2cc05e7c9401992c879f5e48b039a2
cxumol <cxumol@users.noreply.github.com>
2023-11-09 01:55:28 -0800

    Update checker 5-1

--- llmriddles/questions/level5.py
+++ llmriddles/questions/level5.py
@@ -44,12 +44,12 @@ def _checker_1(question_text: str, user_text: str, answer_text: str, lang: str):
     answer_text = answer_text.strip()
     user_text = user_text.strip()
 
+    if count_words(user_text) < 10:
+        return False, "用户的问题长度应该至少10个字" if lang == 'cn' else 'Question should be no less than 10 words.'
+
     if lang == 'en':
         answer_text = answer_text.split()
         user_text = user_text.split()
-
-    if count_words(user_text) < 10:
-        return False, "用户的问题长度应该至少10个字" if lang == 'cn' else 'Question should be no less than 10 words.'
     
     if not any(each in answer_text for each in user_text):
         return True, None
