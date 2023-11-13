e8dd249c08676e8b4d139d4ac3957b88cfa31a5e
Tianyi Peng <tianyipeng95@gmail.com>
2023-11-13 03:46:30 -0500

    feature(tianyipeng): add 1-5, 1-6, 3-4, 4-1, 4-2 (#17)

--- llmriddles/questions/level3.py
+++ llmriddles/questions/level3.py
@@ -1,29 +1,7 @@
 from .question import register_question
-
-
-def count_words(text: str, contain_punctuation: bool = False):
-    chinese_words = []
-    english_words = []
-    other_words = []
-    temp_english_words = []
-    for char in text:
-        if '\u4e00' <= char <= '\u9fff':
-            chinese_words.append(char)
-            if len(temp_english_words) > 0:
-                english_words.append(''.join(temp_english_words))
-                temp_english_words = []
-        else:
-            if char.isalpha():
-                temp_english_words.append(char)
-            else:
-                if len(temp_english_words) > 0:
-                    english_words.append(''.join(temp_english_words))
-                    temp_english_words = []
-                other_words.append(char)
-    if contain_punctuation:
-        return len(chinese_words) + len(english_words) + len(other_words)
-    else:
-        return len(chinese_words) + len(english_words)
+from .question import Checker
+from typing import Callable
+from .utility import count_words, word_reverse_text
 
 
 CN_TEXT_1 = """
@@ -84,14 +62,9 @@ def _checker_2(question_text: str, user_text: str, answer_text: str, lang: str):
     if count_words(user_text) < 10:
         return False, "用户的问题长度应该至少10个字" if lang == 'cn' else 'Question should be no less than 10 words.'
 
-    if lang == 'cn':
-        user_text = user_text[::-1]
-    else:
-        words = user_text.split()
-        words.reverse()
-        user_text = ' '.join(words)
+    reversed_user_text = word_reverse_text(user_text, lang)
 
-    if user_text == answer_text:
+    if reversed_user_text == answer_text:
         return True, None
     else:
         return False, None
@@ -144,22 +117,43 @@ register_question(
     level=3,
 )
 
-# CN_TEXT_4 = """
-# 第三章第四题（回文协变），请你输入一个本身不是回文串的问题，使得正着问和倒着问时，模型的回答本身不是回文且也是逆序。
+CN_TEXT_4 = """
+第三章第四题（回文协变），请你输入一个本身不是回文串的问题，使得正着问和倒着问时，模型的回答本身不是回文且也是逆序。
 
-# 请在下面的输入框内填写你的提问并点击按钮提交。
-# """
+请在下面的输入框内填写你的提问并点击按钮提交。
+"""
 
-# EN_TEXT_4 = """
-# For the fourth question in chapter 3, please enter a question that is not a palindrome string, so that the model's answer is also not a palindrome and is in reverse order when asked forward or backward.
+EN_TEXT_4 = """
+For the fourth question in chapter 3, please enter a question that is not a palindrome string, so that the model's answer is also not a palindrome and is in reverse order when asked forward or backward.
 
-# Please enter your query below and click the submit button
-# """
+Please enter your query below and click the submit button
+"""
 
-# def _checker_4(question_text: str, user_text: str, answer_text: str, lang: str):
-#     pass
+def _checker_4(question_text: str, user_text: str, answer_text: str, lang: str, llm_callback: Callable[[str], str]):
+    answer_text = answer_text.strip()
+    user_text = user_text.strip()
+    reversed_user_text = word_reverse_text(user_text, lang)
+    second_answer_text = llm_callback(reversed_user_text)
 
-# register_question({
-#     'cn': CN_TEXT_4,
-#     'en': EN_TEXT_4,
-# }, _checker_4, level=3)
+    if user_text == reversed_user_text:
+        return False, "用户的问题不能是回文串" if lang == 'cn' else 'Question should not be a palindrome string.'
+
+    if second_answer_text != word_reverse_text(answer_text, lang):
+        return False, f"正着问和倒着问时，模型的回答应该是逆序。\n 用户问题：{user_text}\n 正着问回答: {answer_text} \n 反着问回答: {second_answer_text}" if lang == 'cn' else f'The model\'s answer should be in reverse order when asked forward or backward.\n Question: {user_text}\n Forward-Asking answer: {answer_text} \n Backward-Asking answer: {second_answer_text}'
+
+    if second_answer_text == word_reverse_text(second_answer_text):
+        return False, "正着问和倒着问时，模型的回答不能是回文串" if lang == 'cn' else 'The model\'s answer should not be a palindrome when asked forward or backward.'
+    
+    return True, None
+
+register_question(
+    {
+        'cn': CN_TEXT_4,
+        'en': EN_TEXT_4,
+    }, 
+    checkers=Checker(_checker_4, required_input_keys=['question_text', 'user_text', 'answer_text', 'lang', 'llm_callback']), 
+    name={'cn': '3-4 回文协变', 'en': '3-4'},
+    level=3,
+)
+
+
--- llmriddles/questions/question.py
+++ llmriddles/questions/question.py
@@ -17,21 +17,32 @@ class Question:
 
 _KNOWN_PROBLEMS = []
 
+class Checker:
 
+    def __init__(self, checkers, required_input_keys=None) -> None:
+        self._origin_checkers = checkers
+        if isinstance(checkers, collections.abc.Mapping):
+            self.checker = self._integrated_checker
+        else:
+            self.checker = checkers
+        
+        if required_input_keys == None:
+            required_input_keys = ['question_text', 'user_text', 'answer_text', 'lang']
+        self.required_input_keys = required_input_keys
+
+    def _integrated_checker(self, question_text: str, user_text: str, answer_text: str, lang: str):
+        return self._origin_checkers[lang](question_text, user_text, answer_text)
+
+    def __call__(self, inputs):
+        return self.checker(*[inputs[key] for key in self.required_input_keys])
+        
 def register_question(text: Union[Mapping[str, str], str],
                       checkers: Union[Mapping[str, SingleLangCheckerTyping], MultiLangCheckerTyping],
                       name=Union[Mapping[str, str], str],
                       level: int = 1, default_lang='cn'):
-    if isinstance(checkers, collections.abc.Mapping):
-        _origin_checkers = checkers
-
-        def _integrated_checker(question_text: str, user_text: str, answer_text: str, lang: str):
-            return _origin_checkers[lang](question_text, user_text, answer_text)
-
-        checker: MultiLangCheckerTyping = _integrated_checker
-    else:
-        checker: MultiLangCheckerTyping = checkers
-
+    
+    checker = checkers if isinstance(checkers, Checker) else Checker(checkers)
+        
     if isinstance(text, str):
         texts = {default_lang: text}
     else:
